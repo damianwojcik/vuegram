60b56f9b80935738e63fc846cdf8390129723abe
damianwojcik <khamian@gmail.com>
2020-12-17 18:46:19 +0100

    login/signup/resetPassword components

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { auth } from './firebase'
+import './assets/scss/app.scss'
 
 auth.onAuthStateChanged(() => createApp(App).use(store).use(router).mount('#app'))
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,36 @@
 import { createStore } from 'vuex'
+import * as fb from '../firebase'
+import router from '../router/index'
 
 export default createStore({
   state: {
+    userProfile: {}
   },
   mutations: {
+    setUserProfile(state, payload) {
+      state.userProfile = payload
+    }
   },
   actions: {
+    async login({ dispatch }, form) {
+      const { user } = await fb.auth.signInWithEmailAndPassword(form.email, form.password)
+      dispatch('fetchUserProfile', user)
+    },
+    async fetchUserProfile({ commit }, user) {
+      const userProfile = await fb.usersCollection.doc(user.uid).get()
+      commit('setUserProfile', userProfile.data())
+      router.push('/')
+    },
+    async signup({ dispatch }, form) {
+      const { user } = await fb.auth.createUserWithEmailAndPassword(form.email, form.password)
+    
+      await fb.usersCollection.doc(user.uid).set({
+        name: form.name,
+        title: form.title
+      })
+    
+      dispatch('fetchUserProfile', user)
+    }
   },
   modules: {
   }
